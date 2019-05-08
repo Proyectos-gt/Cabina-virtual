160ed39a0ef31799748731f604e17b3876259375
Jorge Laj <jorge.laj@unitypromotores.com>
2019-05-08 10:03:54 -0600

    agregando funcion al socket para enviar el post

--- js/socket.js
+++ js/socket.js
@@ -4,14 +4,14 @@ var VideoModerador;
 
 var socket = new WebSocket('wss://app.hotelhaciendadelpedregal.com/api/socket');
 
-socket.onopen = function(event) {
+socket.onopen = function (event) {
   //log('Conexion Abiera ');
   var json = JSON.stringify({ Mensaje: 'Esperando Conexion' });
   socket.send(json);
   //log('Sent: ' + json);
 }
 
-socket.onerror = function(event) {
+socket.onerror = function (event) {
   //log('Error: ' + JSON.stringify(event));
 }
 
@@ -20,22 +20,22 @@ function closeWin() {
   VideoModerador.close();
 }
 
-function OpenVideo() { 
+function OpenVideo() {
   var audio = document.getElementById("audio");
   audio.play();
-  VideoModerador = window.open('https://appear.in/unitypromotoresgt',"ventana1","width=750,height=550,scrollbars=NO,location=NO");
+  VideoModerador = window.open('https://appear.in/unitypromotoresgt', "ventana1", "width=750,height=550,scrollbars=NO,location=NO");
 }
 
 
 socket.onmessage = function (event) {
   text = JSON.parse(event.data);
 
-  if(text.Mensaje === "Telus"){
+  if (text.Mensaje === "Telus") {
     $("#moderador").attr("src", "https://appear.in/unitypromotoresgt");
     $("#iconoEmpresa").attr("src", "img/Telus.jpg");
     $("#nombre").text(text.Nombre);
-    $("#telefono").text("Telefono: "+ text.Telefono);
-    $("#empresa").text("Empresa: "+ text.Mensaje);
+    $("#telefono").text("Telefono: " + text.Telefono);
+    $("#empresa").text("Empresa: " + text.Mensaje);
     $("#codigo").text("Codigo: " + text.Codigo);
     $("#buscar-codigo").text(text.Codigo);
     $("#tipo").text("Tipo: " + text.Tipo);
@@ -44,27 +44,47 @@ socket.onmessage = function (event) {
   }
 }
 
-socket.onclose = function(event) {
+socket.onclose = function (event) {
   //log('Closed connection');
 }
 
-$('#enviar').on('click', function(){
-    var json = JSON.stringify(
-      { 
-        Mensaje: 'Telus', 
-        Nombre: $('#name').val(), 
-        Telefono: $('#phone').val(), 
-        Codigo: $('#identificador').val(),
-        Tipo: $("#ddlOpciones option:selected").text()
-      });
+$('#enviar').on('click', function () {
+  var json = JSON.stringify(
+    {
+      Mensaje: 'Telus',
+      Nombre: $('#name').val(),
+      Telefono: $('#phone').val(),
+      Codigo: $('#identificador').val(),
+      Tipo: $("#ddlOpciones option:selected").text()
+    });
 
-    socket.send(json);
-   
-    $('#name').val('');
-    $('#phone').val('');
-    $('#email').val('');
+  socket.send(json);
+
+  $.post("https://encuestas.unitypromotores.com/api/v1/encuesta/guardar",
+    {
+      "table_name": 'cabina_virtual',
+      "nombre": $('#name').val(),
+      "correo": $('#email').val(),
+      "telefono": $('#phone').val(),
+      "codigo": $('#identificador').val()
+    },
+  ).done(function () {
+    setTimeout(
+      function () {
+        console.log('envio exitoso');
+      }, 200);
+  }).fail(function () {
+    setTimeout(
+      function () {
+        console.log('no se pudo enviar');
+      }, 200);
+  });
+
+  $('#name').val('');
+  $('#phone').val('');
+  $('#email').val('');
 });
 
-window.addEventListener('beforeunload', function() {
+window.addEventListener('beforeunload', function () {
   socket.close();
 });
